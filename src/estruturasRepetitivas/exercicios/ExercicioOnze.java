19cc5d242e67fd6b28d3d1ba679664fb5eb23e10
camilazci <camilazucchi@outlook.com>
2024-01-08 17:55:10 -0300

    feat: implement extra exercise eleven (iterative structure)

---
+++ src/estruturasRepetitivas/exercicios/ExercicioOnze.java
@@ -0,0 +1,10 @@
+package estruturasRepetitivas.exercicios;
+
+public class ExercicioOnze {
+    public static void main(String[] args) {
+        /* 11. Escreva um programa que utilize um loop "for" para imprimir os números de 1 a 10. */
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
