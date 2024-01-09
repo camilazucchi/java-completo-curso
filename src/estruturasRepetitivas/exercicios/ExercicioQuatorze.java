72db35945944442c860b87eb6583e792a2e855bd
camilazci <camilazucchi@outlook.com>
2024-01-09 14:07:28 -0300

    feat: implement extra exercise fourteen (do-while)

---
+++ src/estruturasRepetitivas/exercicios/ExercicioQuatorze.java
@@ -0,0 +1,13 @@
+package estruturasRepetitivas.exercicios;
+
+public class ExercicioQuatorze {
+    public static void main(String[] args) {
+        /* 14. Crie um programa que utilize um loop "for" para fazer uma contagem regressiva de 10 até 1.
+        * Em cada iteração, o programa deve imprimir o número atual da contagem. */
+        System.out.println("Contagem regressiva");
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+        System.out.println("Contagem encerrada!");
+    }
+}
